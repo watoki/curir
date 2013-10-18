8049348e59bad5dcb2e24861606d09e26542887f
Nikolas Martens <Nikolas.M@rtens.org>
2013-10-18 07:16:41 +0200

    Added Specification for dynamic routes

---
+++ spec/watoki/curir/resources/DynamicRoutesTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace spec\watoki\curir\resources;
+
+use watoki\scrut\Specification;
+
+class DynamicRoutesTest extends Specification {
+
+    function testFindResourceInDynamicRoute() {
+        $this->markTestIncomplete();
+    }
+
+} 
