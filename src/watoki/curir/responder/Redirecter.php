95e079f84fb0292b3988df4bb09fdb42d730bf64
Nikolas Martens <Nikolas.M@rtens.org>
2013-10-25 12:36:33 +0200

    Made Redirecter use Url instead of Path

--- src/watoki/curir/responder/Redirecter.php
+++ src/watoki/curir/responder/Redirecter.php
@@ -1,18 +1,19 @@
 <?php
 namespace watoki\curir\responder;
 
-use watoki\curir\http\Path;
 use watoki\curir\http\Request;
 use watoki\curir\http\Response;
+use watoki\curir\http\Url;
 use watoki\curir\Resource;
 use watoki\curir\resource\DynamicResource;
 use watoki\curir\Responder;
 
 class Redirecter extends Responder {
 
+    /** @var \watoki\curir\http\Url */
     private $target;
 
-    function __construct(Path $target) {
+    function __construct(Url $target) {
         $this->target = $target;
     }
 
@@ -25,5 +26,19 @@ class Redirecter extends Responder {
         $response = new Response();
         $response->getHeaders()->set(Response::HEADER_LOCATION, $this->target->toString());
         return $response;
+    }
+
+    /**
+     * @return \watoki\curir\http\Url
+     */
+    public function getTarget() {
+        return $this->target;
+    }
+
+    /**
+     * @param \watoki\curir\http\Url $target
+     */
+    public function setTarget($target) {
+        $this->target = $target;
     }
 }
